b9463c3e68a4c83cf7209ab7d64016577cfcde45
Alex Naupay <alexnaupay@gmail.com>
2025-05-01 14:20:23 -0500

    `chore: update dependencies and integrate dotenv for environment configuration`

--- lib/aws-cdk-started-stack.ts
+++ lib/aws-cdk-started-stack.ts
@@ -5,6 +5,8 @@ import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as dynamo from 'aws-cdk-lib/aws-dynamodb'
 import * as apiGateway from 'aws-cdk-lib/aws-apigateway'
 import * as iam from 'aws-cdk-lib/aws-iam'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as process from "node:process";
 
 export class AwsCdkStartedStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -37,6 +39,14 @@ export class AwsCdkStartedStack extends Stack {
                 USERS_TABLE_NAME: dynamoUsersTable.tableName,
             }
         })
+
+        // const vpc = new ec2.Vpc(this, 'MyVpc', )
+        /*const vpc = ec2.Vpc.fromLookup(this, 'MyDefaultVpc', { isDefault: true})  // Default
+        const ec2Server = new ec2.Instance(this, 'Ec2Server', {
+            vpc,
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+            machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+        })*/
 
         //dynamoUsersTable.grantFullAccess(saveUsersLambda)
         dynamoUsersTable.grantReadWriteData(saveUsersLambda)  // Give permissions to lambda to read and write
@@ -71,5 +81,6 @@ export class AwsCdkStartedStack extends Stack {
             value: dynamoUsersTable.tableName,
             description: 'DynamoDB Table Name',
         });
+
     }
 }
